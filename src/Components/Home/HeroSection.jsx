a0b02063e6ede8465d4d459d4b54cb194acba6a3
Varad Sandeep Naik <varad0007@gmail.com>
2025-08-04 17:51:58 +0530

    Completed the calorie calculation logic

--- src/Components/Home/HeroSection.jsx
+++ src/Components/Home/HeroSection.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
 import React from "react";
 import CalorieCard from "../CalorieCard";
 import { FaBowlFood } from "react-icons/fa6";
 
 const HeroSection = () => {
+
+  const [age , setAge] = useState(20);
+  const [weight , setWeight] = useState(68);
+  const [height , setHeight] = useState(178);
+  const [gender , setGender] = useState("Male");
+  const [activityLevel , setActivityLevel] = useState(1.2);
+  const [calories , setCalories] = useState();
+
+  useEffect(()=>{
+    calculateBMR(age,weight,height,activityLevel,gender)
+  },[])
+
+  let result = 0;
+
+  function calculateBMR(age,weight,height,activityLevel,gender)
+  {
+    if(gender == "Male")
+    {
+      result = ((10 * weight) + (6.25 * height) - (5 * age) + 5) * activityLevel
+    }else{
+      result = ((10 * weight) + (6.25 * height) - (5 * age) - 161) * activityLevel
+    }
+    
+    setCalories(result.toFixed(0)) 
+    
+  }
+
   return (
     <>
       <div className="px-20 py-8 flex flex-col gap-y-10">
@@ -17,7 +45,7 @@ const HeroSection = () => {
         </div>
         <div className="flex gap-x-10">
           <div className="w-1/3 px-15 pt-15 pb-10 rounded-2xl border-2 border-primary">
-            <form action="" className="flex flex-col gap-y-10">
+            <form action="" className="flex flex-col gap-y-10" onSubmit={(e)=>{e.preventDefault}}>
               <label htmlFor="" className="flex gap-x-4 items-center">
                 <h4 className="text-2xl">Age : </h4>
                 <input
@@ -26,18 +54,19 @@ const HeroSection = () => {
                   id=""
                   className="flex-1"
                   placeholder="Ages 15-80 Years"
+                  onChange={(e)=>{setAge(e.target.value)}}
                 />
               </label>
 
-              <div className="flex gap-x-5 items-center">
+              <div className="flex gap-x-5 items-center" onChange={(e) => {setGender(e.target.value)}}>
                 <h4 className="text-2xl">Gender : </h4>
                 <label htmlFor="Male" className="flex gap-x-1">
-                  <input type="checkbox" name="" id="Male" />
+                  <input type="radio" name="Gender" value="Male" id="Male" />
                   Male
                 </label>
 
                 <label htmlFor="Female" className="flex gap-x-1 items-center">
-                  <input type="checkbox" name="" id="Female" />
+                  <input type="radio" name="Gender" value="Female" id="Female" />
                   Female
                 </label>
               </div>
@@ -50,6 +79,7 @@ const HeroSection = () => {
                   id=""
                   className="flex-1"
                   placeholder="Kilograms(Kg)"
+                  onChange={(e)=>{setWeight(e.target.value)}}
                 />
               </label>
 
@@ -61,21 +91,25 @@ const HeroSection = () => {
                   id=""
                   className="flex-1"
                   placeholder="Centimeters(cm)"
+                  onChange={(e)=>{setHeight(e.target.value)}}
                 />
               </label>
 
               <label htmlFor="" className="flex gap-x-4 items-center">
                 <h4 className="text-2xl">Activity level : </h4>
-                <select name="" id="" className="flex-1">
-                  <option value="">Basal Metabolic Rate</option>
-                  <option value="">Light Excercise</option>
-                  <option value="">Medium Excercise</option>
-                  <option value="">Heavy Excercise</option>
-                  <option value="">Active Excercise</option>
+                <select name="" id="" className="flex-1" onChange={(e)=>{setActivityLevel(e.target.value)}}>
+                  <option value="1.2">Sedentary</option>
+                  <option value="1.375">Lightly active</option>
+                  <option value="1.55">Moderately active</option>
+                  <option value="1.725">Very active</option>
+                  <option value="1.9">Extra active</option>
                 </select>
               </label>
 
-              <button className="py-5 bg-primary rounded-2xl text-l font-bold text-white cursor-pointer">
+              <button className="py-5 bg-primary rounded-2xl text-l font-bold text-white cursor-pointer" onClick={(e)=>{
+                  e.preventDefault()
+                  calculateBMR(age,weight,height,activityLevel,gender)}
+                }>
                 Calculate Your Maintenance Calories
               </button>
             </form>
@@ -91,17 +125,17 @@ const HeroSection = () => {
               <div className="flex gap-x-5">
                 <CalorieCard
                   CalorieType={"Maintain weight"}
-                  Calories={2400}
+                  Calories={calories}
                   Percentage={100}
                 />
                 <CalorieCard
-                  CalorieType={"Mild weight loss"}
-                  Calories={2175}
+                  CalorieType={"Mild Weight Loss"}
+                  Calories={(calories - (0.1 * calories)).toFixed(0)}
                   Percentage={90}
                 />
                 <CalorieCard
                   CalorieType={"Weight loss"}
-                  Calories={1925}
+                  Calories={(calories - (0.21 * calories)).toFixed(0)}
                   Percentage={79}
                 />
               </div>
